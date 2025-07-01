8d0580491bf8d6ab6cd400a2db78911fe49033cd
IdanHarpaz <harpazidan2007@gmail.com>
2025-07-01 20:08:10 +0300

    fixed settings msg

--- timer-mod/settings.lua
+++ timer-mod/settings.lua
@@ -1,8 +1,8 @@
 data:extend({
   {
-    type = "double-setting",
+    type = "int-setting",
     name = "game-time-minutes",
-    setting_type = "runtime-per-user",
+    setting_type = "runtime-global",
     minimum_value = 1,
     maximum_value = 525600,  -- 1 year
     default_value = 60,
